24d00e32294d1e87935500db0a9cb702423b6508
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2022-09-28 13:52:18 +0300

    Add files via upload

---
+++ 4.1.py
@@ -0,0 +1,19 @@
+from random import randint
+numbers = []
+for i in range(10):
+    numbers.append(randint (-10,10))
+print(numbers)
+
+def get_numbers(numbers):
+    count =0 
+    for element in numbers:
+        count +=1
+    return count
+print('Number of elements: ', get_numbers(numbers))
+
+x = int(input('Введите позицию 1 элемента: '))
+y = int(input('Введите позицию 2 элемента: '))
+
+for i in range(len(numbers)):
+    mult = numbers[x -1]*numbers[y - 1]
+print(f'Mult of elements: {numbers[x -1]} * {numbers[y -1]} =', mult)
